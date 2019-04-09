990abd2d9e039612390845bc259aa7a2393cfbd2
wuriutami <44470490+wuriutami@users.noreply.github.com>
2019-04-09 21:15:39 +0700

    Fungsi save dan import CSV

---
+++ ExportImportCSV.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lib.h"
+
+void savetocsvjadwal(jadwal_t j[15][5]) {
+    /*Membuat file agar hasil dapat dimasukkan ke dalam file csv*/
+    FILE *outfile;
+    outfile = fopen("Tubes_Jadwal Praktikum.csv","w");
+
+    /*Layout jadwal*/
+    fprintf(outfile,"Ruang, Praktikum, LAB1, LAB2, LAB3, LSS\n");
+    fprintf(outfile,"Minggu, Hari\n");
+
+    char day[5][7] = {"Senin","Selasa","Rabu","Kamis","Jumat"};
+
+    /*looping untuk jadwal setiap minggu*/
+    for (int minggu=3; minggu<=15; minggu++) {
+        for (int hari=0; hari<=4; hari++) {
+            /*layout jadwal (setiap baris selalu ada)*/
+            fprintf(outfile,"%d, %s", minggu, day[hari]);
+            /*jadwal praktikum sesuai input*/
+            //asumsi 1: kalau ga ada jadwal, hasilnya kosong
+            //asumsi 2: r=4 untuk validasi
+            /*LAB 1 = lab[r=0]*/
+            fprintf(outfile,"%s-%s", j[minggu][hari].lab[0].prak, j[minggu][hari].lab[0].rom);
+            /*LAB 2 = lab[r=1]*/
+            fprintf(outfile,"%s-%s", j[minggu][hari].lab[1].prak, j[minggu][hari].lab[1].rom);
+            /*LAB 3 = lab[r=2]*/
+            fprintf(outfile,"%s-%s", j[minggu][hari].lab[2].prak, j[minggu][hari].lab[2].rom);
+            /*LAB LSS = lab[r=3]*/
+            fprintf(outfile,"%s-%s\n", j[minggu][hari].lab[3].prak, j[minggu][hari].lab[3].rom);
+        }
+    }
+    fclose(outfile);
+}
+
+void savetocsvasisten(jadwal_t [15][5]) {
+    /*Membuat file agar hasil dapat dimasukkan ke dalam file csv*/
+    FILE *outfile;
+    outfile = fopen("Tubes_Jadwal Asisten.csv","w");
+
+    /*Layout jadwal*/
+    fprintf(outfile,"Ruang, Praktikum, LAB1, LAB2, LAB3, LSS\n");
+    fprintf(outfile,"Minggu, Hari\n");
+
+    char day[5][7] = {"Senin","Selasa","Rabu","Kamis","Jumat"};
+
+    /*looping untuk jadwal setiap minggu*/
+    for (int minggu=3; minggu<=15; minggu++) {
+        for (int hari=0; hari<=4; hari++) {
+            /*layout jadwal (setiap baris selalu ada)*/
+            fprintf(outfile,"%d, %s", minggu, day[hari]);
+            /*jadwal praktikum sesuai input*/
+            /*asumsi: jika array kosong, maka hasil cetaknya kosong*/
+            /*LAB 1 = lab[r=0]*/
+            fprintf(outfile,"%s", j[minggu][hari].lab[0].assist);
+            /*LAB 2 = lab[r=1]*/
+            fprintf(outfile,"%s", j[minggu][hari].lab[1].assist);
+            /*LAB 3 = lab[r=2]*/
+            fprintf(outfile,"%s", j[minggu][hari].lab[2].assist);
+            /*LAB LSS = lab[r=3]*/
+            fprintf(outfile,"%s\n", j[minggu][hari].lab[3].assist);
+
+
+}
+void importcsv() {
+    //variabel
+    char *file;
+    //input user
+    printf("Masukkan nama file, tambahkan .csv: "); scanf("%s", file);
+    //belum beres hehe
+    FILE* inp;
+    inp = fopen(file,"r");
+    if (inp == NULL) {
+        printf("File kosong.");
+    } else {
+        printf("File berhasil dimuat.");
+    }
+}
+
+
